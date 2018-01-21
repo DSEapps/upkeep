b9cdcde18ede89f9073dd0774a0912760887cb6f
esharri2 <esharri2@gmail.com>
2018-01-21 12:21:23 -0500

    implemented Google auth + minimal front end for testing

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+var db = require("../models");
+module.exports = function (app, passport) {
+    //Authentication route
+    app.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+    //Authentication callback
+    app.get('/login/google/callback',
+        passport.authenticate('google', { successRedirect: '/dashboard', failureRedirect: '/' }))        
+
+}
