05630636d2a5265b81f1d67163ecdd312733990e
esharri2 <esharri2@gmail.com>
2018-01-24 20:23:19 -0500

    worked on root and login routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,20 +2,39 @@ var path = require("path");
 var fs = require("fs");
 
 module.exports = function (app) {
-    //Welcome/login page
+    //Initial landing page. Will send user straight to dashboard if authenticated.
     app.get("/", function (req, res) {
-        res.render("index", { test: "testing / route" });
+        if (req.user) {
+            res.redirect("/dashboard")
+        } else {
+            res.render("landing");
+        }
+    });
+
+    app.get("/login", function(req,res){
+        res.render("register");
+    })
+   
+    //Dashboard of maintenance schedule
+    app.get("/dashboard", function (req, res) {
+        //TODO - Add Sequelize query to get all users activities and items            
+        //if user doesn't have any items (by extension, no activities), res.redirect("/setup")
+        //if user doesn't exist, res.redirect to ("/login")
+        //else render dashboard with all the data
+        var obj = {};
+        res.render("dashboard", obj);
     });
 
     //Create profile page for new users
-    app.get("/newprofile", function (req, res) {
+    app.get("/setup", function (req, res) {
         //Fetches data from items.json file and sends to handelbars
         fs.readFile('../db/items.json', "utf8", (err, data) => {
             if (err) throw err;
             console.log(data);
             //EH Note: I'm not sure data needs to be parsed here, haven't tested this code yet.
             var obj = { items: JSON.parse(data) };
-            res.render("profile", obj);
+            console.log("derp");
+            res.render("setup", obj);
         });
     });
 
@@ -27,11 +46,6 @@ module.exports = function (app) {
         res.render("profile", obj);
     })
 
-    //Dashboard of maintenance schedule
-    app.get("/dashboard", function (req, res) {
-        //TODO - Add Sequelize query to get users activities        
-        var obj = {};
-        res.render("dashboard", obj);
-    });
+
 };
 
