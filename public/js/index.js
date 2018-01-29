b381b38d0800b4cd8c747f6212d4a072f3f5bb17
esharri2 <esharri2@gmail.com>
2018-01-28 19:05:46 -0500

    created submit jquery for setupdetails

--- public/js/index.js
+++ public/js/index.js
@@ -21,36 +21,66 @@ $(document).ready(function () {
     // SETUPDETAILS
     // Setupdetails event listener
     $("#btn-itemsDetails").on("click", function (event) {
-        // grab values from each item
-        // item-type
-        // item-manufacturer
-        // item-serial
-        // item-note
-        // task-name
-        // last-performed
+        var profile = { items: [], tasks: [] }
+        var tasksNoTimes = "";
 
+        function Item(id, brand, serial, notes) {
+            this.item_id = id;
+            this.manufacturer = brand;
+            this.serial_number = serial;
+            this.items_note = notes;
+        }
+
+        $(".item-wrapper").map(function () {
+            var id = $(this).attr("data-id");
+            var brand = $(this).find("input[name='brand']").val();
+            var serial = $(this).find("input[name='serial']").val();
+            var note = $(this).find("textarea[name='notes']").val();
+            var newitem = new Item(id, brand, serial, note);
+            profile.items.push(newitem);
+        })
+
+        function Task(id, date) {
+            this.task_id = id;
+            this.last_performed = date;
+        }
+
+        $(".item-tasks-block").map(function () {
+            var id = $(this).attr("data-taskid");
+            var time = $(this).find("input[name='last-performed']").val();
+            if (!time) {
+                tasksNoTimes += $(this).find(".task-title").text() + "\n";
+            }
+            var newtask = new Task(id, time);
+            profile.tasks.push(newtask);
+        })
+
+        if (tasksNoTimes.length > 0) {
+            alert("Please enter a last completed date for the following tasks:\n" + tasksNoTimes)
+            return;
+        }
 
         $.ajax("/editdetails", {
             type: "POST",
-            data: JSON.stringify(items),
+            data: JSON.stringify(profile),
             contentType: "application/json"
         }).done(function (url) {
             // api function returns /dashboard
             window.location.replace(url);
-            }
-        );
-    })
+        }
+            );
+    })//End of Setupdetails
 
     //DASHBOARD
 
     // Navigate to setupitems
     $("#btn-editAllitems").on("click", function (event) {
-         window.location.replace("/setupitems");
+        window.location.replace("/setupitems");
     });
 
 
 
-    //End of Setupdetails
+
 
 
 
