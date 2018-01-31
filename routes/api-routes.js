96afaf77426cda319b061ce7f723d0c2422f0adc
esharri2 <esharri2@gmail.com>
2018-01-30 20:27:27 -0500

    remove item is working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,17 +15,21 @@ module.exports = function (app, db, passport) {
     //Takes data from edit OR create new items page and sends user to /setupdetails
     app.post("/edititems", function (req, res) {
         var itemnames = [];
+        var toDeleteIds = [];
+
         db.items.findAll({
             where: {
                 userUserId: req.user.user_id
             }
         }).done(function (items) {
+            //Make array of items user already has
             if (items.length > 0) {
                 items.forEach(function (item) {
                     itemnames.push(item.type)
                 })
             }
 
+            //make an object for the Sequelize bulk create
             var itemsObj = itemsToSQL(req.body, req.user.user_id);
 
             if (itemnames.length > 0) {
@@ -34,12 +38,51 @@ module.exports = function (app, db, passport) {
                         itemsObj.splice(i, 1);
                     }
                 }
+
+
+
+                var toDelete = [];
+                itemnames.forEach(function (itemInDB, index) {
+                    var deleteIt = true;
+                    req.body.forEach(function (checkedItem) {
+                        if (checkedItem === itemInDB) {
+                            deleteIt = false;
+                        }
+                    })
+                    if (deleteIt) {
+                        toDelete.push(itemInDB)
+                    }
+                })
+
+                toDelete.forEach(function (itemToDelete) {
+                    items.forEach(function (itemInDB) {
+                        if (itemToDelete === itemInDB.type) {
+                            toDeleteIds.push(itemInDB.item_id);
+                        }
+                    })
+                })
             }
 
             db.items.bulkCreate(itemsObj).then(function () {
-                console.log("done with bulk create of items");
-                res.send("/setupdetails");
+                if (toDeleteIds.length > 0) {
+                    var delPromises = toDeleteIds.map(function (delId) {
+                        return db.items.destroy({
+                            where: {
+                                item_id: delId
+                            }
+                        })
+                    })
+
+                    Promise.all(delPromises).then(function () {
+                        res.send("/setupdetails");
+                    })
+
+
+                } else {
+                    res.send("/setupdetails");
+                }
             })
+
         })
     })
 
@@ -69,12 +112,10 @@ module.exports = function (app, db, passport) {
         })
 
         Promise.all(itemPromises).then(function (data) {
-            console.log(data);
             var taskPromises = tasks.map(function (task) {
                 return db.tasks.upsert(task)
             })
             Promise.all(taskPromises).then(function (data) {
-                console.log(data);
                 res.send("/dashboard");
             })
         })
